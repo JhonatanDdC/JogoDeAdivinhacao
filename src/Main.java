ac1f919ca6485844ef1545531955fa293b86561c
Jhonatan <jhoncruz.duarte10@gmail.com>
2026-04-14 16:07:14 -0300

    Adicionei um contador de tentativas

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
         int numeroSecreto = random.nextInt(100) + 1;
         int tentativa;
+        int contadorTentativa = 0;
 
         System.out.println("==== Jogo de Adivinhação ====");
         System.out.println("Objetivo: Acertar o número de 1 a 100");
@@ -16,6 +17,7 @@ public class Main {
         do {
             System.out.print("Digite sua tentativa: ");
             tentativa = in.nextInt();
+            contadorTentativa++;
 
             if (tentativa > numeroSecreto){
                 System.out.println("O número é menor!");
@@ -25,7 +27,10 @@ public class Main {
                 System.out.println("Você acertou!");
             }
 
+
         } while (tentativa != numeroSecreto);
+
+        System.out.println("Você utilizou " + contadorTentativa + " tentativas!");
 
         in.close();
     }
